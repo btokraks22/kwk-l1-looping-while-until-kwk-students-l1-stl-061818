0572e85a439aa32f8b39bfaa2f68ff5eb2d74de6
btokraks22 <bree.v.v.13@gmail.com>
2018-06-19 20:57:34 +0000

    automatically backed up by learn

--- while.rb
+++ while.rb
@@ -1,1 +1,26 @@
 def using_while 
+  
+    levitation_force = 6
+  
+  loop do 
+    
+    if levitation_force = 10 
+      break
+      
+  end 
+  
+  puts "Wingardium Leviosa"
+  sleep (0.5)
+  
+  levitation_force = levitation_force + 1 
+  
+  puts "You have #{levitation_force}. Congrats!"
+
+end 
+
+end
+  
+ using_while 
+  
+  
+  
